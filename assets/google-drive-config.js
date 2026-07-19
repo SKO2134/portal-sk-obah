c180029068784509e5ae0b023d07ee451deec19b
SK OBAH <xba2134@moe.edu.my>
2026-07-19 15:45:28 +0800

    Sambungkan galeri dengan Google Drive

--- assets/google-drive-config.js
+++ assets/google-drive-config.js
@@ -1,3 +1,3 @@
 // Tampal URL Web App Google Apps Script selepas deployment.
-export const googleDriveUploadUrl = 'PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL';
+export const googleDriveUploadUrl = 'https://script.google.com/macros/s/AKfycbyMq4r5EAGnYOjK8ziSqU8TWxFZO2B3fRV45uiqBmrpHQKMoJoB4o15v5XOwb7N_2rW/exec';
 export const googleDriveReady = googleDriveUploadUrl.startsWith('https://script.google.com/macros/s/');
